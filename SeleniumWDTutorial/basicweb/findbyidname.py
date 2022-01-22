cc139e7de3fcb17814b9e8057c23db76dfeb4c86
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2022-01-21 18:26:07 -0800

    Find by ID and Name

---
+++ SeleniumWDTutorial/basicweb/findbyidname.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+
+class RunChromeTest():
+    def testMethod(self):
+        global driver
+        serv = Service(
+            "C:\\Users\\astro\\OneDrive\\Documents\\GitHub\\SeleniumProjects\\drivers\\ChromeDriver\\chromedriver.exe")
+        driver = webdriver.Chrome(service=serv)
+        driver.maximize_window()
+        driver.get("https://www.alexemtech.com/practice/")
+        elementbyid = driver.find_element_by_id("name")
+        if elementbyid is not None:
+            print("Found an element by ID")
+
+        elementbyname = driver.find_element_by_name("courses1")
+        if elementbyname is not None:
+            print("Found an element by name")
+        while (True):
+            pass
+        driver.quit()
+
+
+chrome = RunChromeTest()
+chrome.testMethod()
