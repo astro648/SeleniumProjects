e8757438be0c8444b1bc4e1bbfee09f04bdc8d30
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2022-01-22 20:37:14 -0800

    ByClass

---
+++ SeleniumWDTutorial/basicweb/ByClass.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+class ByClass():
+
+    def test(self):
+        global driver
+        serv = Service(
+            "C:\\Users\\astro\\OneDrive\\Documents\\GitHub\\SeleniumProjects\\drivers\\ChromeDriver\\chromedriver.exe")
+        driver = webdriver.Chrome(service=serv)
+        driver.maximize_window()
+        driver.get("https://www.alexemtech.com/practice/")
+
+        elementById = driver.find_element(By.ID,"name")
+
+        if elementById is not None:
+            print("We found an element by Id")
+
+        elementByXpath = driver.find_element(By.XPATH,"//input[@id='displayed-text']")
+
+        if elementByXpath is not None:
+            print("We found an element by XPATH")
+
+        elementByLinkText = driver.find_element(By.LINK_TEXT,"Open Tab")
+
+        if elementByLinkText is not None:
+            print("We found an element by Link Text")
+
+
+        while (True):
+            pass
+
+        driver.quit()
+
+ff = ByDemo()
+ff.test()
