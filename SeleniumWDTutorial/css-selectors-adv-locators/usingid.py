6959403b6c1ee439bdb7d2e5c43fb356dea4aa94
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2022-01-28 17:47:31 -0800

    base

--- SeleniumWDTutorial/css-selectors-adv-locators/usingid.py
+++ SeleniumWDTutorial/css-selectors-adv-locators/usingid.py
@@ -1,3 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
+class usingID():
+    def test(self):
+        global driver
+        serv = Service(
+            "C:\\Users\\astro\\OneDrive\\Documents\\GitHub\\SeleniumProjects\\drivers\\ChromeDriver\\chromedriver.exe")
+        driver = webdriver.Chrome(service=serv)
+        driver.maximize_window()
+        driver.get("https://www.alexemtech.com/practice/")
+
+        elementsByID = driver.find_elements(By.ID, "radio-btn-example")
+        length = len(elementsByID)
+
+        if elementsByID is not None:
+            print("We found",length,"elements by ID")
+
+
+
+        while (True):
+            pass
+
+        driver.quit()
