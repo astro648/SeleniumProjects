9dc17d19ecb49c8bbad2d3cc2491b91220b6d0bf
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2022-02-04 17:54:05 -0800

    import time and pause for some time

--- SeleniumWDTutorial/working-with-web-elements/click-on-webpage.py
+++ SeleniumWDTutorial/working-with-web-elements/click-on-webpage.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import time
 
 
 class RunChromeTest():
@@ -11,13 +12,17 @@ class RunChromeTest():
         driver = webdriver.Chrome(service=serv)
         driver.maximize_window()
         driver.get("http://simplecampus.site/")
+        time.sleep(3)
         email_field = driver.find_element(By.ID, "email")
         password_field = driver.find_element(By.ID, "name")
         login_btn = driver.find_element(By.ID, "loginbtn")
+        time.sleep(3)
         email_field.send_keys("admin")
         password_field.send_keys("admin")  # these two send the word "admin" to the email and password field
+        time.sleep(3)
         email_field.clear() # clears field
         email_field.send_keys("admin")
+        time.sleep(3)
         login_btn.click()
 
         while (True):
