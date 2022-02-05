5894041041f3cbe742f737164f5ba54d538a7544
Ayaan R <53174223+astronomerayaan@users.noreply.github.com>
2022-02-04 17:38:19 -0800

    default code

---
+++ SeleniumWDTutorial/working-with-web-elements/click-on-webpage.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+
+class RunChromeTest():
+    def testMethod(self):
+        global driver
+        serv = Service(
+            "C:\\Users\\astro\\OneDrive\\Documents\\GitHub\\SeleniumProjects\\drivers\\ChromeDriver\\chromedriver.exe")
+        driver = webdriver.Chrome(service=serv)
+        driver.maximize_window()
+        driver.get("https://www.nasa.gov/")
+        while (True):
+            pass
+        driver.quit()
+
+
+chrome = RunChromeTest()
+chrome.testMethod()
